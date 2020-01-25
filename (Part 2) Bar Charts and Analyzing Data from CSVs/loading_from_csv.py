1a0249cc50c871bbd3722e3a9ba0435fd705cd97
Parsa Hosseini <officer47p@gmail.com>
2020-01-25 14:14:50 +0330

    corey's way of showing popular programming languages

--- (Part 2) Bar Charts and Analyzing Data from CSVs/loading_from_csv.py
+++ (Part 2) Bar Charts and Analyzing Data from CSVs/loading_from_csv.py
@@ -1,20 +1,42 @@
 import csv
-import numpy as np
 from matplotlib import pyplot as plt
 from collections import Counter
 
-# print(plt.style.available)
-plt.style.use("fivethirtyeight")
+csv_file = open("./data.csv", "r")
+csv_reader = csv.DictReader(csv_file)
 
-with open("./data.csv", "r") as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+counter = Counter()
 
-    first_row = next(csv_reader)
+for row in csv_reader:
+    counter.update(row["LanguagesWorkedWith"].split(";"))
 
-    c = Counter(first_row["LanguagesWorkedWith"].split(";"))
-    for row in csv_reader:
-        c.update(row["LanguagesWorkedWith"].split(";"))
+print(counter)
 
-    names_x = list(c.keys())
-    plt.bar(names_x, [c[f"{name}"] for name in names_x])
-    plt.show()
+languages_x = []
+popularity_y = []
+
+for item in counter.most_common(15):
+    languages_x.append(item[0])
+    popularity_y.append(item[1])
+
+languages_x.reverse()
+popularity_y.reverse()
+
+plt.barh(languages_x, popularity_y)
+
+# # My own way
+# counter_items = list(counter.items())
+# counter_items.sort(key=lambda item: item[1])
+
+# languages_x = [item[0] for item in counter_items]
+# popularity_y = [item[1] for item in counter_items]
+
+# for lang, pop in zip(languages_x, popularity_y):
+#     plt.barh([lang], [pop], label=lang)
+
+
+# plt.legend()
+plt.xlabel("Popularity")
+plt.ylabel("Programming Languages")
+plt.tight_layout()
+plt.show()
