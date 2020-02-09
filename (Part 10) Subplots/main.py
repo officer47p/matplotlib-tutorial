8e83beb0534d7782d549b964e400e07823e45594
Parsa Hosseini <officer47p@gmail.com>
2020-02-09 17:39:15 +0330

    working with subplots

--- (Part 10) Subplots/main.py
+++ (Part 10) Subplots/main.py
@@ -1,33 +1,41 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 plt.style.use('ggplot')
 
 # plt.xkcd()
 
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+data = pd.read_csv("data.csv")
 
-py_dev_y = [45372, 48876, 53850, 57287, 63016,
-            65998, 70003, 70000, 71496, 75370, 83640]
+ages_x = data["Age"]
+
+py_dev_y = data["Python"]
+js_dev_y = data["JavaScript"]
+dev_y = data["All_Devs"]
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+# fig, ax1 = plt.subplots()
+# fig, ax2 = plt.subplots()
+
+ax1.plot(ages_x, py_dev_y, color="#5a7d9a", linewidth=3, label="Python Devs")
+ax1.plot(ages_x, js_dev_y, color="#c9cc00", linewidth=3, label="JS Devs")
+
+ax2.plot(ages_x, dev_y, color="#444444", linestyle="--", label="All Devs")
 
 
-js_dev_y = [37810, 43515, 46823, 49293, 53437,
-            56373, 62375, 66674, 68745, 68746, 74583]
+ax1.set_xlabel("Ages")
+ax1.set_ylabel("Median Salary (USD)")
+ax1.set_title("Median Salary (USD) by Age")
+ax1.legend()
+ax1.grid(True)
 
+ax2.set_xlabel("Ages")
+ax2.set_ylabel("Median Salary (USD)")
+ax2.set_title("Median Salary (USD) by Age")
+ax2.legend()
+ax2.grid(True)
 
-dev_y = [38496, 42000, 46752, 49320, 53200,
-         56000, 62316, 64928, 67317, 68748, 73752]
-
-plt.plot(ages_x, py_dev_y, color="#5a7d9a", linewidth=3, label="Python Devs")
-plt.plot(ages_x, js_dev_y, color="#c9cc00", linewidth=3, label="JS Devs")
-plt.plot(ages_x, dev_y, color="#444444", linestyle="--", label="All Devs")
-
-
-plt.xlabel("Ages")
-plt.ylabel("Median Salary (USD)")
-plt.title("Median Salary (USD) by Age")
-plt.legend()
 plt.tight_layout()
-plt.grid(True)
-plt.xticks(ages_x)
+# ax.set_xticks(ages_x)
 plt.show()
